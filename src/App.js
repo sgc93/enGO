9ffe6b58c8775e6e360cba6e57b205d1984418fb
sgc93 <smachewgedefaw@gmail.com>
2023-11-06 11:13:35 -0900

    rendering initial items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
 	return (
 		<div className="app">
@@ -32,7 +37,27 @@ function Form() {
 }
 
 function PackingList() {
-	return <div className="list">List</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((item) => (
+					<Item item={item} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({ item }) {
+	console.log(item);
+	return (
+		<li>
+			<span>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats() {
