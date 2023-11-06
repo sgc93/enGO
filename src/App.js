be44bd5c13f99b031ca2b228b5c1518412690069
sgc93 <smachewgedefaw@gmail.com>
2023-11-06 10:44:58 -0900

    update title of the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 function App() {
-	return <h1>ENGO</h1>;
+	return (
+		<div className="app">
+			<Header />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<div className="header">
+			<h1>🎒Pack</h1>
+			<h4>Your Items Properly</h4>
+		</div>
+	);
+}
+
+function Form() {
+	return (
+		<div className="add-form">
+			<h3>What do you need for your trip?</h3>
+		</div>
+	);
+}
+
+function PackingList() {
+	return <div className="list">List</div>;
+}
+
+function Stats() {
+	return (
+		<footer className="stats">
+			<em>
+				🎒 You have X items on your list, and you have already packed Y items
+				(Z%).
+			</em>
+		</footer>
+	);
 }
 
 export default App;
