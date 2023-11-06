7720135b5365c75ed6bb421d32b35fc965f7e067
sgc93 <smachewgedefaw@gmail.com>
2023-11-06 11:33:06 -0900

    update Form component : using form tag

--- src/App.js
+++ src/App.js
@@ -30,9 +30,18 @@ function Header() {
 
 function Form() {
 	return (
-		<div className="add-form">
+		<form className="add-form">
 			<h3>What do you need for your trip?</h3>
-		</div>
+			<input type="text" placeholder="Item..." />
+			<select>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<button>Add</button>
+		</form>
 	);
 }
 
