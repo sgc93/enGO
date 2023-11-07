0794122568d79e015952041200ebb9f2fd877eea
sgc93 <smachewgedefaw@gmail.com>
2023-11-07 04:36:13 -0900

    create a separate file for Header component

---
+++ src/header.js
@@ -0,0 +1,13 @@
+export function Header() {
+	return (
+		<div className="header">
+			<div className="logo">
+				<img src="assets/images/logo.png" alt="logo"></img>
+			</div>
+			<div className="slogan">
+				<h1>🎒Pack</h1>
+				<h4>Your Items Properly</h4>
+			</div>
+		</div>
+	);
+}
