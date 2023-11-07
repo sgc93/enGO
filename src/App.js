f994221d6bccbabe33139dbcb46f340d809f1b5e
sgc93 <smachewgedefaw@gmail.com>
2023-11-07 04:41:38 -0900

    move Stats component to separate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Header } from "./header.js";
 import { Form } from "./form.js";
+import { Header } from "./header.js";
+import { Stats } from "./stats.js";
 
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: true },
@@ -105,33 +106,6 @@ function Item({ item, onDeleteItem, onPacking }) {
 			</span>
 			<button onClick={() => onDeleteItem(item.id)}>❌</button>
 		</li>
-	);
-}
-
-function Stats({ items }) {
-	if (!items.length) {
-		return (
-			<p className="stats">
-				{" "}
-				<em>Start add items in your packing list 🚀</em>
-			</p>
-		);
-	}
-
-	const numOfItems = items.length;
-	const numOfPacked = items.filter((item) => item.packed).length;
-	const packedPercent = Math.round((numOfPacked / numOfItems) * 100);
-	return (
-		<footer className="stats">
-			<em>
-				{packedPercent === 100
-					? "You have Packed all your Items, READY TO GO! ✈️🌍"
-					: `🎒 You have ${numOfItems} items on your list,`}
-				{numOfPacked === 0
-					? "but You haven't started packing yet, START PACKING!"
-					: ` and you have already packed ${numOfPacked} items ( ${packedPercent}%).`}
-			</em>
-		</footer>
 	);
 }
 
