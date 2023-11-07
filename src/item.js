111d2919a2bd55e6e1f01831e24b3162e157e6db
sgc93 <smachewgedefaw@gmail.com>
2023-11-07 05:08:21 -0900

    create a separate file Item component

---
+++ src/item.js
@@ -0,0 +1,16 @@
+export function Item({ item, onDeleteItem, onPacking }) {
+	// const [isChecked, SetIsChecked] = useState(item.packed);
+	return (
+		<li>
+			<input
+				type="checkbox"
+				checked={item.packed}
+				onChange={() => onPacking(item.id)}
+			></input>
+			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button onClick={() => onDeleteItem(item.id)}>❌</button>
+		</li>
+	);
+}
