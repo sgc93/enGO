5a4b733867451af36d7590f84bbcb61d26d3011d
sgc93 <smachewgedefaw@gmail.com>
2023-11-07 04:11:45 -0900

    handle sorting Paking item list

--- src/App.js
+++ src/App.js
@@ -104,21 +104,32 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onDeleteItem, onPacking }) {
-	const [sort, setSort] = useState("sort by input status");
+	const [sortBy, setSortBy] = useState("input");
+	let sortedItems;
+
+	if (sortBy === "input") {
+		sortedItems = items;
+	} else if (sortBy === "description") {
+		sortedItems = items
+			.slice()
+			.sort((a, b) => a.description.localeCompare(b.description));
+	} else if (sortBy === "packed") {
+		sortedItems = items.slice().sort((a, b) => a.packed - b.packed);
+	}
 	return (
 		<div className="list">
 			<div className="actions">
 				<select
-					value={sort}
-					onChange={(e) => setSort((sort) => e.target.value)}
+					value={sortBy}
+					onChange={(e) => setSortBy((sortBy) => e.target.value)}
 				>
 					<option value="input">sort by input status</option>
 					<option value="description">Sort by item description</option>
-					<option>Sort by Packed Status</option>
+					<option value="packed">Sort by Packed Status</option>
 				</select>
 			</div>
 			<ul>
-				{items.map((item) => (
+				{sortedItems.map((item) => (
 					<Item
 						item={item}
 						onDeleteItem={onDeleteItem}
