b57c15b9b1aef02dec4d2aef39a001ec0563b063
sgc93 <smachewgedefaw@gmail.com>
2023-11-07 04:46:03 -0900

    move PackingList component to  separate file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Form } from "./form.js";
 import { Header } from "./header.js";
 import { Stats } from "./stats.js";
-import { Item } from "./item.js";
+import { PackingList } from "./packingList.js";
 
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: true },
@@ -48,47 +48,6 @@ function App() {
 				onClearList={clearPackingList}
 			/>
 			<Stats items={items} />
-		</div>
-	);
-}
-
-function PackingList({ items, onDeleteItem, onPacking, onClearList }) {
-	const [sortBy, setSortBy] = useState("input");
-	let sortedItems;
-
-	if (sortBy === "input") {
-		sortedItems = items;
-	} else if (sortBy === "description") {
-		sortedItems = items
-			.slice()
-			.sort((a, b) => a.description.localeCompare(b.description));
-	} else if (sortBy === "packed") {
-		sortedItems = items.slice().sort((a, b) => a.packed - b.packed);
-	}
-
-	return (
-		<div className="list">
-			<div className="actions">
-				<select
-					value={sortBy}
-					onChange={(e) => setSortBy((sortBy) => e.target.value)}
-				>
-					<option value="input">sort by input status</option>
-					<option value="description">Sort by item description</option>
-					<option value="packed">Sort by Packed Status</option>
-				</select>
-				<button onClick={onClearList}>clear</button>
-			</div>
-			<ul>
-				{sortedItems.map((item) => (
-					<Item
-						item={item}
-						onDeleteItem={onDeleteItem}
-						onPacking={onPacking}
-						key={item.id}
-					/>
-				))}
-			</ul>
 		</div>
 	);
 }
