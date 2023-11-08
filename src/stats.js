af10a4221e3986a67699c10fe21202cb5810d094
sgc93 <smachewgedefaw@gmail.com>
2023-11-07 22:44:41 -0900

    update list statistics statement

--- src/stats.js
+++ src/stats.js
@@ -15,11 +15,12 @@ export function Stats({ items }) {
 		<footer className="stats">
 			<em>
 				{packedPercent === 100
-					? "You have Packed all your Items, READY TO GO! ✈️🌍"
-					: `🎒 You have ${numOfItems} items on your list,`}
-				{numOfPacked === 0
-					? "but You haven't started packing yet, START PACKING!"
-					: ` and you have already packed ${numOfPacked} items ( ${packedPercent}%).`}
+					? `You have Packed all your ${numOfItems} Items, READY TO GO! ✈️🌍`
+					: `🎒 You have ${numOfItems} items on your list, ${
+							numOfPacked === 0
+								? "but You haven't started packing yet, START PACKING!"
+								: ` and you have already packed ${numOfPacked} items ( ${packedPercent}%).`
+					  }`}
 			</em>
 		</footer>
 	);
